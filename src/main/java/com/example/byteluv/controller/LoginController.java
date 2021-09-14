c7619b2e940fe0b984d42a2adbbc1c616e5ea4ef
MrWang98 <w1968953844@163.com>
2021-09-14 15:50:10 +0800

    init

--- src/main/java/com/example/byteluv/controller/LoginController.java
+++ src/main/java/com/example/byteluv/controller/LoginController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.util.StringUtils;
 
 @Controller
 @Slf4j
@@ -22,31 +23,31 @@ public class LoginController {
 
     @GetMapping("/login")
     public String login() {
-//        if (StringUtils.isEmpty(user.getUserName()) || StringUtils.isEmpty(user.getPassword())) {
-//            return "login";
-//        }
-//        //用户认证信息
-//        Subject subject = SecurityUtils.getSubject();
-//        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(
-//                user.getUserName(),
-//                user.getPassword()
-//        );
-//        try {
-//            //进行验证，这里可以捕获异常，然后返回对应信息
-//            subject.login(usernamePasswordToken);
-//            subject.checkRole("admin");
-//            subject.checkPermissions("query", "add");
-//        } catch (UnknownAccountException e) {
-//            log.error("用户名不存在！", e);
-//            return "用户名不存在！";
-//        } catch (AuthenticationException e) {
-//            log.error("账号或密码错误！", e);
-//            return "账号或密码错误！";
-//        } catch (AuthorizationException e) {
-//            log.error("没有权限！", e);
-//            return "没有权限";
-//        }
-//        return "login success";
+        if (StringUtils.isEmpty(user.getUserName()) || StringUtils.isEmpty(user.getPassword())) {
+            return "login";
+        }
+        //用户认证信息
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(
+                user.getUserName(),
+                user.getPassword()
+        );
+        try {
+            //进行验证，这里可以捕获异常，然后返回对应信息
+            subject.login(usernamePasswordToken);
+            subject.checkRole("admin");
+            subject.checkPermissions("query", "add");
+        } catch (UnknownAccountException e) {
+            log.error("用户名不存在！", e);
+            return "用户名不存在！";
+        } catch (AuthenticationException e) {
+            log.error("账号或密码错误！", e);
+            return "账号或密码错误！";
+        } catch (AuthorizationException e) {
+            log.error("没有权限！", e);
+            return "没有权限";
+        }
+        return "login success";
         return "/login";
     }
 
